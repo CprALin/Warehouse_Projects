4598edf82d8ab5bf04ae003fb9e857782c5c5bdd
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2025-06-22 22:39:53 +0300

    Update advance_data_analytics.sql

--- ADA/advance_data_analytics.sql
+++ ADA/advance_data_analytics.sql
@@ -83,3 +83,21 @@ CASE WHEN current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORD
 END py_change
 FROM yearly_product_sales
 ORDER BY product_name , order_year
+
+
+-- Which categories contribute the most to overall sales ? 
+WITH category_sales AS (
+SELECT 
+	category,
+	SUM(sales_amount) AS total_sales
+FROM gold.fact_sales f 
+LEFT JOIN gold.dim_product p ON p.product_key = f.product_key
+GROUP BY category
+)
+SELECT 
+	category,
+	total_sales,
+	SUM(total_sales) OVER () overall_sales,
+	CONCAT(ROUND((CAST(total_sales AS FLOAT) / SUM(total_sales) OVER ()) * 100 , 2) , '%') AS percentage_of_total
+FROM category_sales
+ORDER BY total_sales DESC
