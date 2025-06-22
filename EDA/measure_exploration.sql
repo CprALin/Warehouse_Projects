d43d12f94540d5f7fa2c8625875d6516128789a6
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2025-06-22 21:12:21 +0300

    Update measure_exploration.sql

--- EDA/measure_exploration.sql
+++ EDA/measure_exploration.sql
@@ -128,3 +128,56 @@ LEFT JOIN gold.dim_customers c ON c.customer_id = f.customer_id
 GROUP BY 
 	c.country
 ORDER BY total_sold_items DESC
+
+
+-- Which 5 products generate the highest revenue ? 
+SELECT TOP 5
+	p.product_name,
+	SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_product p ON p.product_key = f.product_key
+GROUP BY p.product_name 
+ORDER BY total_revenue DESC
+
+
+-- What are the 5 worst - performing products in terms of sales ? 
+SELECT * 
+FROM (
+	SELECT
+		p.product_name,
+		SUM(f.sales_amount) AS total_revenue,
+		RANK() OVER (ORDER BY SUM(f.sales_amount) DESC) AS rank_products
+	FROM gold.fact_sales f 
+	LEFT JOIN gold.dim_product p ON p.product_key = f.product_key
+	GROUP BY p.product_name
+)t 
+WHERE rank_products <= 5 
+
+
+-- Find the top 10 customers who have generated the highest revenue 
+SELECT TOP 10
+	c.customer_key,
+	c.first_name,
+	c.last_name,
+	SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f 
+LEFT JOIN gold.dim_customers c ON c.customer_id = f.customer_id
+GROUP BY 
+	c.customer_key,
+	c.first_name,
+	c.last_name
+ORDER BY total_revenue DESC
+
+-- The 3 customers with the fewest order placed
+SELECT TOP 3
+	c.customer_key,
+	c.first_name,
+	c.last_name,
+	COUNT(DISTINCT order_number) AS total_orders
+FROM gold.fact_sales f 
+LEFT JOIN gold.dim_customers c ON c.customer_id = f.customer_id
+GROUP BY 
+	c.customer_key,
+	c.first_name,
+	c.last_name
+ORDER BY total_orders 
